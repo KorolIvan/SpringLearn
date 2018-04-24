5ad4fe9bbcb87e5bd1b7d907b951801bc42c99a1
Ivan Korol <korol.vanja2008@gmail.com>
2018-04-24 15:55:13 +0300

    app class with main method

---
+++ src/main/java/learn/spring/api/CourseApiApp.java
@@ -0,0 +1,14 @@
+package learn.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ivan Korol on 4/24/2018
+ */
+@SpringBootApplication
+public class CourseApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApp.class, args);
+    }
+}
